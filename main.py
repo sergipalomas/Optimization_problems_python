3e0bb543ab347525cc83ba31fea294e696664570
spalomas <sergi.palomas@bsc.es>
2021-04-26 00:17:21 +0200

    Upload report

---
+++ main.py
@@ -0,0 +1,9 @@
+from TOML_exercises import ex2, ex3, ex4, ex5, ex6, ex7, ex8
+
+def main():
+    # Just call the run function from any of the exercises
+    # ex1 is an exception. Run it as a regular python script
+    ex3.run()
+
+if __name__ == "__main__":
+    main()
